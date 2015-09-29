b6608d1ea98b80f2b3d86f3c95b2f874aeb50074
Rytov Vasily <freeman2103@gmail.com>
2015-09-29 22:34:14 +0500

    Classes, DB, arch.
    Vasyan petrosyan.

---
+++ army/WpfApplication1/Class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+namespace WpfApplication1
+{
+
+
+    public class Question
+    {
+        [Indexed, MaxLength(1000)]
+        public string subject { get; set; }
+        [Indexed, MaxLength(1000)]
+        public string answer { get; set; }
+        [Indexed, MaxLength(5)]
+        public string type { get; set; }
+    }
+
+    public class User
+    {
+        [Indexed, MaxLength(100)]
+        public string Name { get; set; }
+        [Indexed, MaxLength(100)]
+        public string Soname{ get; set; }
+        [Indexed, MaxLength(100)]
+        public string Thname{ get; set; }
+        [Indexed]
+        public int devision;
+    }
+
+
+
+}
---
+++ army/WpfApplication1/Database.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows;
+using SQLite3 = System.Data.SQLite;
+using SQLite;
+
+namespace WpfApplication1
+{
+    public static class Database
+    {
+        public static void Login(User user)
+        {
+                  
+            if (!File.Exists("database.db"))
+            {
+                SQLite3.SQLiteConnection.CreateFile("database.db");
+            }
+            try
+            {
+                //TODO: общение с базой и продумать базу
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        
+
+        public static bool Answer(Question q, string ans)
+        {
+            return false;
+        }
+
+
+
+    }
+}
